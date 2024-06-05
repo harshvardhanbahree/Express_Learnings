ef392067da4b41e31bfab5433aa5c4691470ba23
harsh vardhan <harshvardhanbahree@gmail.com>
2024-06-05 20:36:55 +0530

    Express and middleware

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-awsref;
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+//middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .put((req, res) => {
+    res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    res.json({ status: "Pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  users.push({ ...req.body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
+});
+app.listen(port, () => {
+  console.info("server is listening");
+});
