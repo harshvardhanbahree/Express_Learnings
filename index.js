bef680eaad7bcb1c73a96cdbd1bd851ea602fa02
harsh vardhan <harshvardhanbahree@gmail.com>
2024-06-05 20:42:30 +0530

    Express and middleware

--- index.js
+++ index.js
@@ -10,19 +10,11 @@ app.get("/users", (req, res) => {
   return res.json(users);
 });
 
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-  })
-  .put((req, res) => {
-    res.json({ status: "Pending" });
-  })
-  .delete((req, res) => {
-    res.json({ status: "Pending" });
-  });
+app.route("/api/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
 
 app.post("/api/users", (req, res) => {
   users.push({ ...req.body, id: users.length + 1 });
